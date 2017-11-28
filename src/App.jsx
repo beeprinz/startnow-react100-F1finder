cbe07bea32f55d7fa4cb00b3319d5f3421ddd389
Brendan Prinz <brendanprinz@gmail.com>
2017-11-27 19:14:35 -0800

    adds initial project files

--- src/App.jsx
+++ src/App.jsx
@@ -1,95 +1,90 @@
 import React, { Component } from "react";
-
 import Alert from './Alert'; 
+
+const axios = require("axios");
 
 class App extends Component { //parent component
   constructor(props) {
     super(props);
 
     this.state = {
-      text:'',
-      priority:'0',
-      todos: [],    
+      raceTracks: [],    
     };
 
     this.count = 0;
-    this.handleText = this.handleText.bind(this);
-    this.handlePriority = this.handlePriority.bind(this);
+    
+    this.handleGrandPrixName = this.handleGrandPrixName.bind(this);
     this.handleTodo = this.handleTodo.bind(this);
-    this.handleSave = this.handleSave.bind(this);
-    this.handleDelete = this.handleDelete.bind(this);
   }
 
-  handleText(e) {
-    this.setState({ text: e.target.value });
+  componentWillMount() {
+    axios
+      .get("http://ergast.com/api/f1/2017.json")
+      .then(response => {
+        let races = response.data.MRData.RaceTable.Races;
+        races = races.map((race) => {
+          const raceInfo = {
+            name: race.raceName,
+            date: race.date,
+            url: race.url,
+            circuit: {
+              name: race.Circuit.circuitName,
+              url: race.Circuit.url,
+              lat: race.Circuit.Location.lat,
+              lon: race.Circuit.Location.long,
+              country: race.Circuit.Location.country,
+              city: race.Circuit.Location.locality,
+            }
+          }
+
+          return raceInfo;
+        });
+
+        return races;
+      })
+      .then(races => this.setState({ raceTracks: races }));
   }
 
-  handlePriority(e) {
-    this.setState({ priority: e.target.value });
-  }
-
-  handleSave(id, text, priority) {
-    console.log('console log', id, text, priority)
-      var newState = this.state.todos.slice(); //creates copy of array of todos 
-      console.log('slice method test', newState); 
-      
-        var indexId = newState.findIndex((todoCurrentValue) => todoCurrentValue.id === id);
-        //matching the original todo id with the edited todo id
-        console.log('indexId console log', indexId)
-        console.log(this.state.todos[indexId]);
-
-         newState[indexId] = {id: id, text: text, priority: priority}; 
-         this.setState({todos: newState});
-
-      //todoCurrentValue is each individual todo in the todos array. 
-      //newState variable is the sliced todos array that is a copy of the original  
-  }  
-
-  handleDelete(id){
-    var deleteState = this.state.todos.slice();
-
-    var deleteIndexId = deleteState.findIndex((todoCurrentValue) => todoCurrentValue.id === id); 
-
-    deleteState.splice(deleteIndexId, 1);
-    this.setState({todos: deleteState});
+  handleGrandPrixName(e) {
+    this.setState({ GrandPrixName: e.target.value });
   }
 
   handleTodo(e) {
     e.preventDefault();
-    if (this.state.priority === '0') {
+    if (this.state.GrandPrixName === '0') {
       return false;
     }
-
-    const userText = this.state.text;
-    const userPriority = this.state.priority;
-    const todos = this.state.todos;
-
-    todos.push({
-      id: this.count++,
-      text: userText,
-      priority: userPriority,
-      editEnable: false 
-    });
-
-    this.setState({
-      todos:todos 
-    })
   }
+    // const userText = this.state.text;
+    // const userPriority = this.state.priority;
+    // const todos = this.state.todos;
+
+    // todos.push({
+    //   id: this.count++,
+    //   text: userText,
+    //   priority: userPriority,
+    //   editEnable: false 
+    // });
+
+    // this.setState({
+    //   todos:todos 
+    // })
+  
 
   render() {
     return (
       <div>
         <div className="container">
-          <h1>Very Simple Todo App</h1>
-          <p>Track all of the things</p>
+          <h1>Formula 1 Track Locator</h1>
+          <p>Discover Circuits Around the World!</p>
           <div className="row">
             <div className="col-md-4">
               <div className="card">
-                <div className="card-header">Enter Information</div>
+                {/* <div className="card-header">Enter Information</div> */}
                 <div className="card-body">
                   <div>
                     <form onSubmit={this.handleTodo}>
-                      <div className="form-group">
+                      {/* <div className="form-group">
                         <label htmlFor="textArea">I want to...</label>
                         <textarea
                           className="form-control create-todo-text" 
@@ -97,21 +92,21 @@ class App extends Component { //parent component
                           rows="3"
                           onChange={this.handleText}
                         />
-                      </div>
+                      </div> */}
                       <div className="form-group">
                         <label htmlFor="exampleFormControlSelect1">
-                          How much of a priority is this?!
+                          Choose a Grand Prix
                         </label>
                         <select
                           className="mt-2 mb-4 form-control create-todo-priority"
                           id="exampleFormControlSelect1"
-                          onChange={this.handlePriority} 
+                          onChange={this.handleGrandPrixName} 
                           defaultValue={this.state}
                         >
-                          <option value="0" name="priority">Select Priority</option>  
-                          <option value="high" name="priority">high</option>
-                          <option value="medium" name="priority">medium</option>
-                          <option value="low" name="priority">low</option>
+                        <option value="0" name="grandPrixName">Select Race</option>  
+                        { this.state.raceTracks && this.state.raceTracks.map((race, index) => {
+                          return <option key={index} >{race.name}</option>
+                        })}
                         </select>
                       </div>
                     
@@ -119,7 +114,7 @@ class App extends Component { //parent component
                         type="submit"
                         className="btn btn-primary btn-block create-todo"
                       >
-                        Add
+                        Find Track!
                       </button>
                     </form>
                   </div>
@@ -128,21 +123,24 @@ class App extends Component { //parent component
             </div>
             <div className="col-md-8">
               <div className="card">
-                <div className="card-header">View Todos!</div>
+                <div className="card-header">Circuit Info</div>
                 <div className="card-body" />
                 <div className="container">
-                  {this.state.todos.map((todo, index) => {
+                <p>
+                  
+                </p>
+                  {/* {this.state.todos.map((todo, index) => {
                     //console.log(index);
                     return <Alert
                       todo={todo}
                       key={index}
-                      onSave={this.handleSave} 
-                      onDelete={this.handleDelete}
+                      //onSave={this.handleSave} 
+                      //onDelete={this.handleDelete}
                       //these are all props that are being rendered in alert
                     />
                     //are we going to put the id of the todo in this function?
                   })
-                  }
+                  } */}
                </div>
               </div>
             </div>
--- src/alert.jsx
+++ src/alert.jsx
@@ -4,109 +4,97 @@ class Alert extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      editEnabled: false,
-      text:'',
-      priority: ''
+      // editEnabled: false,
+      // text:'',
+      // priority: ''
     }
     this.handleEdit = this.handleEdit.bind(this);
     this.handleEditTodo = this.handleEditTodo.bind(this);
     this.handleEditTodoPriority = this.handleEditTodoPriority.bind(this);
-    this.handleSave = this.handleSave.bind(this);
-    this.handleDelete = this.handleDelete.bind(this);
+    // this.handleSave = this.handleSave.bind(this);
+    // this.handleDelete = this.handleDelete.bind(this);
   
   }
 
-  handleEdit(e) {
-    this.setState({ editEnabled: true });
-  }
+  // handleEdit(e) {
+  //   this.setState({ editEnabled: true });
+  // }
 
 
   handleEditTodo(e){
     this.setState({ text: e.target.value });
   }
 
-  handleEditTodoPriority(e) {
-    this.setState({ priority: e.target.value });
-  }
+  // handleEditTodoPriority(e) {
+  //   this.setState({ priority: e.target.value });
+  // }
   
-  handleSave(e){
-    this.props.onSave(this.props.todo.id, this.state.text, this.state.priority);
-    this.setState({editEnabled: false}) 
-  //save needs to update the state of the original todo, 
-  //make a copy of todos and match the id of the edited todo to the original 
-  //then setState to the edited version 
-  }
+  // handleSave(e){
+  //   this.props.onSave(this.props.todo.id, this.state.text, this.state.priority);
+  //   this.setState({editEnabled: false}) 
+  // //save needs to update the state of the original todo, 
+  // //make a copy of todos and match the id of the edited todo to the original 
+  // //then setState to the edited version 
+  // }
 
 
-  handleDelete(e) {
-    this.props.onDelete(this.props.todo.id);
-  }
+  // handleDelete(e) {
+  //   this.props.onDelete(this.props.todo.id);
+  // }
 
 
 
   render() {
-    let priority;
-    if (this.props.todo.priority == "medium") {
-      priority = "alert-warning";
-    } else if (this.props.todo.priority == "high") {
-      priority = "alert-danger";
-    } else {
-      priority = "alert-success";
-    }
+    // let priority;
+    // if (this.props.todo.priority == "medium") {
+    //   priority = "alert-warning";
+    // } else if (this.props.todo.priority == "high") {
+    //   priority = "alert-danger";
+    // } else {
+    //   priority = "alert-success";
+    // }
     
-    if (this.state.editEnabled === true){
-      return (
-        <div className={`alert ${priority}`} role="alert" >
-          <label htmlFor="textArea">Description</label> 
-          <textarea 
-            className="update-todo-text" 
-            id="textArea"
-            rows="3" 
-            defaultValue={this.props.todo.text} //defaultvale lets you change the value 
-            onChange={this.handleEditTodo}
-          />
-          <div className="form-group">
-           <label htmlFor="exampleFormControlSelect1">
-            How much of a priority is this?!
-            </label>
-            <select
-              className="mt-2 mb-4 form-control create-todo-priority"
-              id="exampleFormControlSelect1"
-              onChange={this.handleEditTodoPriority} 
-                >
-              <option defaultValue="0" name="priority">Select Priority</option>  
-              <option defaultValue="high" name="priority">high</option>
-              <option defaultValue="medium" name="priority">medium</option>
-              <option defaultValue="low" name="priority">low</option>
-            </select>
-          </div>
-          <button
-            onClick={this.handleSave}
-            type="submit"
-            className="btn btn-primary btn-block">
-            Save
-          </button>
-        </div>
-      ) 
-    }
+    // if (this.state.editEnabled === true){
+    //   return (
+    //     <div className={`alert ${priority}`} role="alert" >
+    //       <label htmlFor="textArea">Description</label> 
+    //       <textarea 
+    //         className="update-todo-text" 
+    //         id="textArea"
+    //         rows="3" 
+    //         defaultValue={this.props.todo.text} //defaultvale lets you change the value 
+    //         onChange={this.handleEditTodo}
+    //       />
+    //       <div className="form-group">
+    //        <label htmlFor="exampleFormControlSelect1">
+    //         How much of a priority is this?!
+    //         </label>
+    //         <select
+    //           className="mt-2 mb-4 form-control create-todo-priority"
+    //           id="exampleFormControlSelect1"
+    //           onChange={this.handleEditTodoPriority} 
+    //             >
+    //           <option defaultValue="0" name="priority">Select Priority</option>  
+    //           <option defaultValue="high" name="priority">high</option>
+    //           <option defaultValue="medium" name="priority">medium</option>
+    //           <option defaultValue="low" name="priority">low</option>
+    //         </select>
+    //       </div>
+    //       <button
+    //         onClick={this.handleSave}
+    //         type="submit"
+    //         className="btn btn-primary btn-block">
+    //         Save
+    //       </button>
+    //     </div>
+    //   ) 
+    // }
 
     return (
       <div> 
-        <div className={`alert ${priority}`} role="alert" id="success">
           <p>
-            <input type="checkbox" />
             {this.props.todo.text}
-            <button onClick={this.handleEdit} className="mx-2 btn btn-primary edit-todo" role="button">
-              Edit
-            </button>
-            <button 
-            className="btn btn-primary delete-todo" 
-            role="button"
-            onClick={this.handleDelete}>
-              Delete
-            </button>
           </p>
-       </div>
       </div>
     );
   }
